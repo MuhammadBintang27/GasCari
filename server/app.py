324d701f9d8e22c32422605431de1e1d8961e8d8
MuhammadBintang27 <muhammad.bintang.indra@gmail.com>
2025-06-15 12:42:16 +0700

    update app.py

--- server/app.py
+++ server/app.py
@@ -174,4 +174,4 @@ def search():
     return jsonify(results)
 if __name__ == '__main__':
     import os
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=False)
